9a598797691bd01b0075cefbee1d5d6922d42dd4
zrma <bulbitain@nhnnext.org>
2013-09-25 23:24:47 -0700

    add basic memory leak check setting

--- oldboy/oldboy/oldboy.cpp
+++ oldboy/oldboy/oldboy.cpp
@@ -5,10 +5,15 @@
 */
 
 #include "stdafx.h"
+#include <crtdbg.h>
 
+#ifdef _DEBUG
+#define	new new(_CLIENT_BLOCK, __FILE__, __LINE__)
+#endif
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	getchar();
 

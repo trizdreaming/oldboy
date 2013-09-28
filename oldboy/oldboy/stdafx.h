c8ef6ad03a7815ae4cdc70de7b73cd62b103d14b
trizdreaming <dcflight@naver.com>
2013-09-28 13:28:17 -0700

    win32console program to win32 project oldboy.cpp conflicted now find some way clearing.

--- oldboy/oldboy/stdafx.h
+++ oldboy/oldboy/stdafx.h
@@ -7,9 +7,15 @@
 
 #include "targetver.h"
 
-#include <stdio.h>
-#include <tchar.h>
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일:
+#include <windows.h>
 
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.

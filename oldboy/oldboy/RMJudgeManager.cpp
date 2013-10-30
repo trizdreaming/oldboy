b9f93d692d8a9ecd3d1cb55e210c4488ec29da1b
flashscope <android_apps@limecolor.com>
2013-10-30 16:14:48 +0900

    판정매니저에서 노트 관리 함수 추가

--- oldboy/oldboy/RMJudgeManager.cpp
+++ oldboy/oldboy/RMJudgeManager.cpp
@@ -2,6 +2,7 @@
 #include "oldboy.h"
 #include "RMjudgeManager.h"
 #include "RMobjectManager.h"
+#include "RMobject.h"
 
 CRMjudgeManager* CRMjudgeManager::m_pInstance = nullptr;
 
@@ -42,11 +43,17 @@ void CRMjudgeManager::StartNote( PLAYER_NUMBER player )
 	{
 	case PLAYER_ONE:
 		auto& iter = CRMobjectManager::GetInstance()->GetObjectList( LAYER_MEMORY_PULL )->begin();
+		(*iter)->SetObjectType(OBJECT_NOTE_NORMAL_1);
+		(*iter)->SetPosition(395, -100);
+		(*iter)->SetSceneType(SCENE_PLAY);
 		CRMobjectManager::GetInstance()->AddObject( *iter , LAYER_NOTE1 );
 		CRMobjectManager::GetInstance()->GetObjectList( LAYER_MEMORY_PULL )->remove(0);
 		break;
 	case PLAYER_TWO:
 		auto& iter = CRMobjectManager::GetInstance()->GetObjectList( LAYER_MEMORY_PULL )->begin();
+		(*iter)->SetObjectType(OBJECT_NOTE_NORMAL_1);
+		(*iter)->SetPosition(910, -100);
+		(*iter)->SetSceneType(SCENE_PLAY);
 		CRMobjectManager::GetInstance()->AddObject( *iter , LAYER_NOTE2 );
 		CRMobjectManager::GetInstance()->GetObjectList( LAYER_MEMORY_PULL )->remove(0);
 		break;

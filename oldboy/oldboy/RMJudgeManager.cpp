441dd5815f8d29a8c656b7c5ec3805a6da46e99f
zrma <bulbitain@nhnnext.org>
2013-10-30 02:26:38 -0700

    판별 계획 주석으로 추가

--- oldboy/oldboy/RMJudgeManager.cpp
+++ oldboy/oldboy/RMJudgeManager.cpp
@@ -75,6 +75,23 @@ void CRMjudgeManager::JudgeNote()
 	{
 		auto& iterP1 = CRMobjectManager::GetInstance()->GetObjectList( LAYER_NOTE1 )->begin();
 		auto thisNoteP1 = iterP1;
+
+		/*
+			1. 노트한테 너 위치 어디야?
+
+			2. 위치가 판별 범위 안이면 
+			키보드 매니저한테 묻기 => 눌렸냐?  -> 구현해야 할 것 - 인풋매니저의 키 버튼 푸시
+
+			3. 눌렸으면?
+			
+			3-1 하단의 사라짐 코드처럼 마찬가지로 메모리풀로 넣어줌
+
+			3-2 판별 => 노트의 종류를 겟 
+
+			3-3 이펙트 발동! (by 신동찬 - "char형 비트연산으로 하겠습니다.")
+		*/
+
+		// 메모리 풀로 넣어주는 코드
 		if ( (*thisNoteP1)->GetPositionY() > SCREEN_SIZE_Y-1 )
 		{
 			CRMobjectManager::GetInstance()->AddObject( *thisNoteP1 , LAYER_MEMORY_POOL );

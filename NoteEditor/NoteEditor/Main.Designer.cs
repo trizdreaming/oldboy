1c3426d520b1558f4c8f5d5571b80f8c48ad79cb
zrma <bulbitain@nhnnext.org>
2013-11-17 03:27:25 -0800

    음원 파일 불러와서 재생하는 기능까지 추가

---
+++ NoteEditor/NoteEditor/Main.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace NoteEditor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장하기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.음원ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mp3Label = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.음원ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.불러오기ToolStripMenuItem,
+            this.저장하기ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(41, 20);
+            this.toolStripMenuItem1.Text = "파일";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // 불러오기ToolStripMenuItem
+            // 
+            this.불러오기ToolStripMenuItem.Name = "불러오기ToolStripMenuItem";
+            this.불러오기ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.불러오기ToolStripMenuItem.Text = "불러오기";
+            // 
+            // 저장하기ToolStripMenuItem
+            // 
+            this.저장하기ToolStripMenuItem.Name = "저장하기ToolStripMenuItem";
+            this.저장하기ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.저장하기ToolStripMenuItem.Text = "저장하기";
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // 음원ToolStripMenuItem
+            // 
+            this.음원ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.불러오기ToolStripMenuItem1});
+            this.음원ToolStripMenuItem.Name = "음원ToolStripMenuItem";
+            this.음원ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.음원ToolStripMenuItem.Text = "음원";
+            // 
+            // 불러오기ToolStripMenuItem1
+            // 
+            this.불러오기ToolStripMenuItem1.Name = "불러오기ToolStripMenuItem1";
+            this.불러오기ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.불러오기ToolStripMenuItem1.Text = "불러오기";
+            this.불러오기ToolStripMenuItem1.Click += new System.EventHandler(this.불러오기ToolStripMenuItem1_Click);
+            // 
+            // mp3Label
+            // 
+            this.mp3Label.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.mp3Label.Location = new System.Drawing.Point(0, 24);
+            this.mp3Label.Name = "mp3Label";
+            this.mp3Label.Padding = new System.Windows.Forms.Padding(5);
+            this.mp3Label.Size = new System.Drawing.Size(1010, 80);
+            this.mp3Label.TabIndex = 1;
+            this.mp3Label.Text = "음원 파일 : 없음";
+            this.mp3Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1008, 627);
+            this.Controls.Add(this.mp3Label);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Note Editor 0.1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장하기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 음원ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem1;
+        private System.Windows.Forms.Label mp3Label;
+    }
+}
+
---
+++ NoteEditor/NoteEditor/Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+// mp3 재생을 위한 추가
+
+namespace NoteEditor
+{
+    public partial class Main : Form
+    {
+        // C코드로 작성된 DLL을 부르기 위함
+        // C#에서 지원하는게 아니므로 네이티브 코드에서 작성된 코드를 활용
+        [DllImport("winmm.dll")]
+        // DLL 속에 있는 이 메소드를 사용하겠다.
+        // 헤더파일(.h)에서 제공해 주는 것만 사용
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        string mp3FileName = string.Empty;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void 불러오기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string fileName = string.Empty;
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory; // + "\\Music\\";
+                dlg.Filter = "mp3 파일 (*.mp3)|*.mp3|모든 파일 (*.*)|*.*";
+                dlg.FilterIndex = 1;
+                dlg.RestoreDirectory = true;
+ 
+                if (dlg.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                mp3FileName = dlg.FileName;
+                mp3Label.Text = "음원 파일 : " + mp3FileName;
+
+                // 재생 코드
+                mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+                mciSendString("open \"" + mp3FileName + "\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+                mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+         
+                // tbxFilePath.Text = mp3FileName;
+                // 추가 한 파일 경로(및 이름)를 보여줌
+            }
+            // FilenameTextbox.Text = openFileDialog1.FileName;
+            // button3.Enabled = true; // So you dont play no file. lol
+            
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

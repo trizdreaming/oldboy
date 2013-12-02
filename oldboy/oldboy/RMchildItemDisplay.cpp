be05e7a5714c23a50a9d0333bb263928b0570381
zrma <bulbitain@nhnnext.org>
2013-12-02 23:23:49 +0900

    테스트 프린트코드 주석처리

--- oldboy/oldboy/RMchildItemDisplay.cpp
+++ oldboy/oldboy/RMchildItemDisplay.cpp
@@ -18,5 +18,5 @@ void CRMchildItemDisplay::Update()
 	m_ObjectType = CRMitemManager::GetInstance()->GetObjectType(m_ThisTier);
 	m_PositionY = CRMitemManager::GetInstance()->GetStackPosition(m_ThisTier);
 
-	printConsole("티어 : %d, 오브젝트 타입 : %d, 좌표 : (%f, %f) \n", m_ThisTier, m_ObjectType, m_PositionX, m_PositionY);
+	// printConsole("티어 : %d, 오브젝트 타입 : %d, 좌표 : (%f, %f) \n", m_ThisTier, m_ObjectType, m_PositionX, m_PositionY);
 }

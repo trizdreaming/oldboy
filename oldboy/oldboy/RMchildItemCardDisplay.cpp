784666ed368cdeb217788fddafff4a752667d521
trizdreaming <dcflight@naver.com>
2013-12-05 00:29:50 +0900

    회전 매트릭스 주석 처리

--- oldboy/oldboy/RMchildItemCardDisplay.cpp
+++ oldboy/oldboy/RMchildItemCardDisplay.cpp
@@ -25,9 +25,12 @@ void CRMchildItemCardDisplay::Render()
 	// 원래 좌표축으로 돌리기 위한 현재 좌표축 임시 저장
 	CRMrender::GetInstance()->GetRenderTarget()->GetTransform( &m_PrevMatrix );
 
+	// 물체 원래 좌표 값 임시 저장
 	float prevPositionX = m_PositionX;
 	float prevPositionY = m_PositionY;
 
+	//물체를 좌표 축 (0,0) 위치로 이동 -> 회전 -> 원래 위치로 회복하도록 함
+	//매트릭스 초기화
 	D2D1::IdentityMatrix();
 
 	if ( m_Width != 0 && m_Height != 0 )
@@ -44,13 +47,13 @@ void CRMchildItemCardDisplay::Render()
 
 	CRMrender::GetInstance()->GetRenderTarget()->SetTransform( m_Matrix );
 
-	//부모의 렌더함수를 빌려서 바로 적용하도록 함
-
+	//부모의 렌더함수를 빌려서 변환 매트릭스 적용하도록 함
 	CRMobject::Render();
 
 	//원래의 좌표축으로 돌려 놓는 것
 	CRMrender::GetInstance()->GetRenderTarget()->SetTransform( m_PrevMatrix );
 
+	//물체가 원래 있던 좌표로 되돌림
 	m_PositionX = prevPositionX;
 	m_PositionY = prevPositionY;
 }

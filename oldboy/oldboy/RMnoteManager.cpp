2af197703922a32b5928fb11ed64bc1938cfa323
flashscope <android_apps@limecolor.com>
2013-12-10 01:41:07 +0900

    따다닥 아이템 구현1

--- oldboy/oldboy/RMnoteManager.cpp
+++ oldboy/oldboy/RMnoteManager.cpp
@@ -24,6 +24,8 @@ CRMnoteManager::CRMnoteManager(void):
 	m_P2NoteIndex(0)
 {
 	ZeroMemory(&m_IsItemDelayOn, sizeof(m_IsItemDelayOn));
+	ZeroMemory(&m_IsItemDuplicateOn, sizeof(m_IsItemDuplicateOn));
+	ZeroMemory(&m_IsItemDuplicateReady, sizeof(m_IsItemDuplicateReady));
 }
 
 
@@ -49,6 +51,8 @@ void CRMnoteManager::Initialize()
 	m_P1NoteIndex = 0;
 	m_P2NoteIndex = 0;
 	ZeroMemory(&m_IsItemDelayOn, sizeof(m_IsItemDelayOn));
+	ZeroMemory(&m_IsItemDuplicateOn, sizeof(m_IsItemDuplicateOn));
+	ZeroMemory(&m_IsItemDuplicateReady, sizeof(m_IsItemDuplicateReady));
 }
 
 void CRMnoteManager::StartNote()
@@ -99,6 +103,45 @@ void CRMnoteManager::StartNote()
 	{
 		m_ElapsedTime = timeGetTime() - m_GameStartTime;
 
+		//////////////////////////////////////////////////////////////////////////
+		// 아이템 구현중인 부분 (따다닥) 중복 리펙토링 후 통합 노트 간격 검사 부분 테스트 필요
+		// 우와앙 디버깅 테스트 힘들어...
+		if ( m_IsItemDuplicateOn[PLAYER_ONE] &&  m_IsItemDuplicateReady[PLAYER_ONE] )
+		{
+			int m_P1NoteMiddleTime = ( m_P1NoteReadyTime - m_P1NoteBeforeTime ) / 2;
+			m_P1NoteMiddleTime += m_P1NoteBeforeTime;
+
+			if ( m_ElapsedTime > (m_P1NoteMiddleTime + m_PausedTime) )
+			{
+				int noteTurm = (m_P1NoteMiddleTime - m_P1NoteBeforeTime);
+				printf_s("delay :%d \n",noteTurm);
+				if ( noteTurm > 100 )
+				{
+					CRMjudgeManager::GetInstance()->StartNote( PLAYER_ONE , m_P1NoteReadyType );
+					m_P1NoteBeforeTime = m_P1NoteMiddleTime;
+					m_IsItemDuplicateReady[PLAYER_ONE] = false;
+				}
+			}
+		}
+		if ( m_IsItemDuplicateOn[PLAYER_TWO] &&  m_IsItemDuplicateReady[PLAYER_TWO] )
+		{
+			int m_P2NoteMiddleTime = ( m_P2NoteReadyTime - m_P2NoteBeforeTime ) / 2;
+			m_P2NoteMiddleTime += m_P2NoteBeforeTime;
+
+			if ( m_ElapsedTime > (m_P2NoteMiddleTime + m_PausedTime) )
+			{
+				int noteTurm = (m_P2NoteMiddleTime - m_P2NoteBeforeTime);
+				if ( noteTurm > 100 )
+				{
+					CRMjudgeManager::GetInstance()->StartNote( PLAYER_TWO , m_P1NoteReadyType );
+					m_P2NoteBeforeTime = m_P2NoteMiddleTime;
+					m_IsItemDuplicateReady[PLAYER_TWO] = false;
+				}
+			}
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+
 		if ( m_ElapsedTime > (m_P1NoteReadyTime + m_PausedTime) )
 		{
 			// 너무 붙어 있는 노트는 생략
@@ -107,6 +150,7 @@ void CRMnoteManager::StartNote()
 			{
 				CRMjudgeManager::GetInstance()->StartNote( PLAYER_ONE , m_P1NoteReadyType );
 				m_P1NoteBeforeTime = m_P1NoteReadyTime;
+				m_IsItemDuplicateReady[PLAYER_ONE] = true;
 			}
 			m_P1NoteReadyTime = UINT_MAX;
 		}
@@ -117,10 +161,12 @@ void CRMnoteManager::StartNote()
 			{
 				CRMjudgeManager::GetInstance()->StartNote( PLAYER_TWO , m_P2NoteReadyType );
 				m_P2NoteBeforeTime = m_P2NoteReadyTime;
+				m_IsItemDuplicateReady[PLAYER_TWO] = true;
 			}
 			m_P2NoteReadyTime = UINT_MAX;
 		}
 	}
+
 }
 
 void CRMnoteManager::StartItemDelay( PlayerNumber player )
@@ -131,4 +177,16 @@ void CRMnoteManager::StartItemDelay( PlayerNumber player )
 void CRMnoteManager::StopItemDelay( PlayerNumber player )
 {
 	m_IsItemDelayOn[player] = false;
+}
+
+void CRMnoteManager::StartItemDuplicate( PlayerNumber player )
+{
+	m_IsItemDuplicateOn[player] = true;
+	m_IsItemDuplicateReady[player] = true;
+}
+
+void CRMnoteManager::StopItemDuplicate( PlayerNumber player )
+{
+	m_IsItemDuplicateOn[player] = false;
+	m_IsItemDuplicateReady[player] = false;
 }

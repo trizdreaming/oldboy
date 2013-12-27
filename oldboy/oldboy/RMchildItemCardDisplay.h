932d6957a5f4edd786bf73363310fb3d427307f8
zrma <bulbitain@nhnnext.org>
2013-12-27 16:59:40 +0900

    Revert "아이템 발동 시 카드 연출 효과 변경하였음 - 좌표 등 세부 조정 필요"
    This reverts commit 4526d1e3d811cc5258e17c909134938ca6579df1.

--- oldboy/oldboy/RMchildItemCardDisplay.h
+++ oldboy/oldboy/RMchildItemCardDisplay.h
@@ -9,26 +9,15 @@ public:
 	virtual ~CRMchildItemCardDisplay(void);
 
 public:
-	virtual void	Update();
-	virtual void	Render();
+	void			Update();
 	virtual void	SetPosition(float x, float y);
 
 private:
 	UINT			m_PrevTime;
 	UINT			m_TimeSlice;
-	
-	bool			m_OneTimeFinishFlag;
-	// 한 번만 보여주기 위한 플래그
-
-	bool			m_Phase1Flag;
-	bool			m_Phase2Flag;
-	bool			m_Phase3Flag;
 	bool			m_FlickFlag;
 
-	UINT			m_FlickCount;
-
 	float			m_WidthOriginal;
-	float			m_HeightOriginal;
 	float			m_PositionXOriginal;
 	float			m_PositionYOriginal;
 };

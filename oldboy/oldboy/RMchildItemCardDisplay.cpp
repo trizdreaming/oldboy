36b2c2bbf8889f34437e5d8ae4fca2c1031b1765
zrma <bulbitain@nhnnext.org>
2013-12-27 17:00:46 +0900

    Revert "Revert "아이템 발동 시 카드 연출 효과 변경하였음 - 좌표 등 세부 조정 필요""
    This reverts commit 932d6957a5f4edd786bf73363310fb3d427307f8.

--- oldboy/oldboy/RMchildItemCardDisplay.cpp
+++ oldboy/oldboy/RMchildItemCardDisplay.cpp
@@ -11,10 +11,16 @@
 CRMchildItemCardDisplay::CRMchildItemCardDisplay(void):
 	m_TimeSlice(30),
 	m_PrevTime(0),
-	m_FlickFlag(true),
+	m_Phase1Flag(false),
+	m_Phase2Flag(false),
+	m_Phase3Flag(false),
 	m_PositionXOriginal(0.0f),
 	m_PositionYOriginal(0.0f),
-	m_WidthOriginal(0.0f)
+	m_WidthOriginal(0.0f),
+	m_HeightOriginal(0.0f),
+	m_FlickCount(0),
+	m_FlickFlag(false),
+	m_OneTimeFinishFlag(false)
 {
 }
 
@@ -40,43 +46,132 @@ void CRMchildItemCardDisplay::Update()
 
 	if ( CRMitemManager::GetInstance()->GetActivatedItem(m_PlayerNumber) == ITEM_TYPE_NONE )
 	{
+		m_Visible = m_Phase1Flag = m_Phase2Flag = m_Phase3Flag = m_OneTimeFinishFlag = false;
+		return;
+	}
+
+	if ( m_OneTimeFinishFlag == true )
+	{
 		m_Visible = false;
-		 //m_Rotation = 0;
+		return;
+	}
+
+	if ( CRMitemManager::GetInstance()->GetActivatedItem(m_PlayerNumber) != ITEM_TYPE_NONE
+		&& m_Phase1Flag == false && m_Phase2Flag == false && m_Phase3Flag == false )
+	{
+		m_WidgetType = CRMitemManager::GetInstance()->GetWidgetTypeOfCard(m_PlayerNumber);
+		m_Phase1Flag = true;
+		m_OneTimeFinishFlag = false;
+
+		m_PositionX = m_PositionXOriginal;
+		m_PositionY = m_PositionYOriginal;
+
+		m_Width = 0.0f;
+		m_Height = 0.0f;
+		m_Alpha = 0.5f;
+		m_FlickFlag = false;
+		m_FlickCount = 0;
 
 		return;
 	}
 
-	m_WidgetType = CRMitemManager::GetInstance()->GetWidgetTypeOfCard(m_PlayerNumber);
-
 	UINT	thisTime = timeGetTime();
 
-	if( m_PrevTime + m_TimeSlice < thisTime )
+	if ( m_Phase1Flag == true )
 	{
-		if( m_FlickFlag == true )
+		if ( m_PrevTime + m_TimeSlice < thisTime )
+		{
+			m_Alpha += 0.05f;
+			m_Width += m_WidthOriginal / 10;
+			m_Height += m_HeightOriginal / 10;
+			m_PositionX -= m_WidthOriginal / 20;
+			m_PositionY -= m_HeightOriginal / 20;
+
+			if ( m_Alpha > 0.98f)
+			{
+				m_Phase1Flag = false;
+				m_Phase2Flag = true;
+			}
+
+			m_PrevTime = thisTime;
+		}
+	}
+	if ( m_Phase2Flag == true )
+	{
+		if ( m_PrevTime + m_TimeSlice < thisTime )
+		{
+			if ( m_FlickFlag )
+			{
+				m_Alpha -= 0.05f;
+
+				if ( m_Alpha > 1.0f )
+				{
+					m_FlickFlag = false;
+				}
+			}
+			else
+			{
+				m_Alpha += 0.05f;
+
+				if ( m_Alpha < 0.5f )
+				{
+					m_FlickFlag = true;
+				}
+			}
+			
+			if ( m_FlickCount > 10)
+			{
+				m_Phase2Flag = false;
+				m_Phase3Flag = true;
+			}
+
+			m_PrevTime = thisTime;
+			++m_FlickCount;
+		}
+	}
+	if ( m_Phase3Flag == true )
+	{
+		if ( m_PrevTime + m_TimeSlice < thisTime )
 		{
 			m_Alpha -= 0.1f;
-			if( m_Alpha <= 0.0f )
-			{
-				m_FlickFlag = false;
-			}
-		}
-		else
-		{
-			m_Alpha += 0.2f;
-			if ( m_Alpha >= 1.0f )
-			{
-				m_FlickFlag = true;
-			}
-		}
-		//printConsole("알파 값: %f \n", m_Alpha);
-		m_PrevTime = thisTime;
+			m_Width += m_WidthOriginal / 10;
+			m_Height += m_HeightOriginal / 10;
+			m_PositionX -= m_WidthOriginal / 20;
+			m_PositionY -= m_HeightOriginal / 20;
 
+			if ( m_Alpha < 0.1f )
+			{
+				m_Phase3Flag = false;
+				m_PositionX = m_PositionXOriginal;
+				m_PositionY = m_PositionYOriginal;
+				m_Width = m_WidthOriginal;
+				m_Height = m_HeightOriginal;
+				m_Alpha = 1.0f;
+				m_OneTimeFinishFlag = true;
+			}
+
+			m_PrevTime = thisTime;
+		}
 	}
 }
 
 void CRMchildItemCardDisplay::SetPosition( float x, float y )
 {
 	CRMobject::SetPosition(x, y);
-	m_PositionXOriginal = x;
-	m_PositionYOriginal = y;
+	m_PositionXOriginal = m_PositionX;
+	m_PositionYOriginal = m_PositionY;
+}
+
+void CRMchildItemCardDisplay::Render()
+{
+	CRMobject::Render();
+
+	if ( m_WidthOriginal == 0 && m_Width != 0)
+	{
+		m_WidthOriginal = m_Width;
+	}
+	if ( m_HeightOriginal == 0 && m_Height != 0)
+	{
+		m_HeightOriginal = m_Height;
+	}
 }
--- oldboy/oldboy/RMchildItemCardDisplay.h
+++ oldboy/oldboy/RMchildItemCardDisplay.h
@@ -9,15 +9,26 @@ public:
 	virtual ~CRMchildItemCardDisplay(void);
 
 public:
-	void			Update();
+	virtual void	Update();
+	virtual void	Render();
 	virtual void	SetPosition(float x, float y);
 
 private:
 	UINT			m_PrevTime;
 	UINT			m_TimeSlice;
+	
+	bool			m_OneTimeFinishFlag;
+	// 한 번만 보여주기 위한 플래그
+
+	bool			m_Phase1Flag;
+	bool			m_Phase2Flag;
+	bool			m_Phase3Flag;
 	bool			m_FlickFlag;
 
+	UINT			m_FlickCount;
+
 	float			m_WidthOriginal;
+	float			m_HeightOriginal;
 	float			m_PositionXOriginal;
 	float			m_PositionYOriginal;
 };
